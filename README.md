bc0d9a8990174755c7d1525b3aeb1bbb8857554a
jerome3o <jeromeswannack@gmail.com>
2020-09-22 08:50:19 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-q7mhxg-kmgbaa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-q7mhxg-kmgbaa)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share(): void {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify(name: string): void {
+    window.alert(`You will be notified when this item goes on sale ${name}`);
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
